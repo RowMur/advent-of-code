4e52c1e1158fc0b70cae1c0f90e1fab49041f7ba
Rowan Murray <rowanomurray@gmail.com>
2024-12-05 12:50:54 +0000

    2024 day 5 pt 2

--- 2024/day_05/main.c
+++ 2024/day_05/main.c
@@ -15,7 +15,8 @@ int main()
     // First array is each number
     // Second array is the numbers that must occur before the first
     int rules[100][100] = {};
-    int summed_middle_pages = 0;
+    int summed_correct_middle_pages = 0;
+    int summed_incorrect_middle_pages = 0;
 
     const unsigned MAX_LENGTH = 256;
     char buffer[MAX_LENGTH];
@@ -72,15 +73,41 @@ int main()
             int middle_page_index = ((page_count + 1) / 2) - 1;
             int middle_val = page_order[middle_page_index];
             printf("Adding %d\n", middle_val);
-            summed_middle_pages += page_order[middle_page_index];
+            summed_correct_middle_pages += page_order[middle_page_index];
         }
         else
         {
             printf("Incorrect\n");
+
+            int expected_left_right_count = ((page_count + 1) / 2) - 1;
+            for (int i = 0; i < page_count; i++)
+            {
+                int left_count = 0;
+                int right_count = 0;
+                int first_num = page_order[i];
+                for (int j = 0; j < page_count; j++)
+                {
+                    int second_num = page_order[j];
+                    if (rules[first_num][second_num])
+                    {
+                        left_count++;
+                    }
+                    if (rules[second_num][first_num])
+                    {
+                        right_count++;
+                    }
+                }
+
+                if (left_count == expected_left_right_count && right_count == expected_left_right_count)
+                {
+                    summed_incorrect_middle_pages += first_num;
+                }
+            }
         }
     }
 
-    printf("Summed correct middle pages: %d", summed_middle_pages);
+    printf("Summed correct middle pages: %d\n", summed_correct_middle_pages);
+    printf("Summed incorrect middle pages: %d\n", summed_incorrect_middle_pages);
 
     fclose(file);
     return 0;
